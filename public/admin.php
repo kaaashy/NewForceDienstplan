6db42c339f7d6e82053097ede2b361ca2db4b61f
Konstantin Kronfeldner <konstantin@boxelware.de>
2024-10-02 16:13:59 +0200

    Improved styling for admin.php page

--- public/admin.php
+++ public/admin.php
@@ -34,49 +34,57 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html>
 <head>
     <title>Login</title>
+    <meta charset="utf-8">
+    <title>Dienstplan NewForce</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <h1>Dev Maintenance</h1>
-    
-    <h2>Mitarbeitende Anlegen</h2>
-    <form method="POST" action="">
-        <div>
-            <label for="username">Name:</label>
-            <input type="text" id="username" name="username" required>
+    <div class="narrowwrapper">
+        <div class="info_page">
+        
+            <h1>Dev Maintenance</h1>
+
+            <h2>Mitarbeitende Anlegen</h2>
+            <form method="POST" action="">
+                <div>
+                    <label for="username">Name:</label>
+                    <input type="text" id="username" name="username" required>
+                </div>
+                <div>
+                    <label for="password">Passwort:</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+
+                <!-- Form 2 fields -->
+                <input type="submit" name="createuser" value="Anlegen">
+            </form>
+
+            <h2>Mitarbeitende Löschen</h2>
+            <form method="POST" action="">
+                <div>
+                    <label for="username">Name:</label>
+                    <input type="text" id="username" name="username" required>
+                </div>
+
+                <!-- Form 2 fields -->
+                <input type="submit" name="deleteuser" value="Löschen">
+            </form>
+
+
+            <h2>Logout</h2>
+            <form method="POST" action="">
+                <!-- Form 2 fields -->
+                <input type="submit" name="logout" value="Ausloggen">
+            </form>
+
+            <h2>Auf Werkseinstellungen Zurücksetzen</h2>
+            <form method="POST" action="">
+                <!-- Form 1 fields -->
+                <input type="submit" name="reinit_everything" value="Auf Werkseinstellungen zurücksetzen">
+            </form>
         </div>
-        <div>
-            <label for="password">Passwort:</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-
-        <!-- Form 2 fields -->
-        <input type="submit" name="createuser" value="Anlegen">
-    </form>
-
-    <h2>Mitarbeitende Löschen</h2>
-    <form method="POST" action="">
-        <div>
-            <label for="username">Name:</label>
-            <input type="text" id="username" name="username" required>
-        </div>
-
-        <!-- Form 2 fields -->
-        <input type="submit" name="deleteuser" value="Löschen">
-    </form>
-
-    
-    <h2>Logout</h2>
-    <form method="POST" action="">
-        <!-- Form 2 fields -->
-        <input type="submit" name="logout" value="Ausloggen">
-    </form>
-
-    <h2>Auf Werkseinstellungen Zurücksetzen</h2>
-    <form method="POST" action="">
-        <!-- Form 1 fields -->
-        <input type="submit" name="reinit_everything" value="Auf Werkseinstellungen zurücksetzen">
-    </form>
-
+    </div>
 </body>
 </html>
 
